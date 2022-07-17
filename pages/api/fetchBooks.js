4546a581a038328e3f084d274a8ac58c08fbbad9
Rinkesh <rinkeshr007@gmail.com>
2022-07-17 21:30:59 +0530

    Vercel api changes

--- pages/api/fetchBooks.js
+++ pages/api/fetchBooks.js
@@ -1,9 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import * as fs from 'fs';
+//  Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readFile(`./pages/api/json/books/${req.query.bookid}.json`,'utf-8',(err,data)=>{
-    res.status(200).json(JSON.parse(data))
-    console.log(err);
-  })
+// export default function handler(req, res) {
+//   fs.readFile(`./pages/api/json/books/${req.query.bookid}.json`,'utf-8',(err,data)=>{
+//     res.status(200).json(JSON.parse(data))
+//     console.log(err);
+//   })
+// }
+
+
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export default async function handler(req, res) {
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), 'json');
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + `/${req.query.bookid}.json`, 'utf8');
+  //Return the content of the data file in json format
+  res.status(200).json(fileContents);
 }
